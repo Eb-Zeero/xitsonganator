9c13792ac08cdc1fc87804510b4dbe55407d2428
Eb-Zeero <macebelene@gmail.com>
2018-04-06 11:13:15 +0200

    Home info component and basic test

---
+++ src/components/HomeInfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h1 class="title is-2">Xitsonga</h1>
+    <p>{{message}}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'this is some Info about Xitsonga'
+    }
+  }
+}
+</script>
+<style scoped></style>
---
+++ test/unit/specs/HomeInfo.spec.js
@@ -0,0 +1,20 @@
+import { shallow } from '@vue/test-utils'
+import HomeInfo from '@/components/HomeInfo'
+
+describe('HomeInfo', () => {
+  it('renders a message and responds correctly to user input', () => {
+    const wrapper = shallow(HomeInfo, {
+      data: {
+        word: 'this is some massage',
+      }
+    })
+
+    // see if the message renders
+    expect(wrapper.find('.title.is-2').text()).toEqual('Xitsonga')
+  })
+
+  // Inspect the raw component options
+  it('has a data hook', () => {
+    expect(typeof HomeInfo.data).toBe('function')
+  })
+})
